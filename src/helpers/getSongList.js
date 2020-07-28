b3c7854108d5c3d0a91de8810947f2f80a7f21f4
Johnrambo <barrars@gmail.com>
2020-07-28 01:36:05 -0700

    fix

---
+++ src/helpers/getSongList.js
@@ -0,0 +1,40 @@
+
+/**
+ *
+ *
+ * @param {LocalForage} myDB
+ */
+export default function getSongList (setSongList, myDB ) {
+  // fetch('http://localhost:3001/songlist')
+  fetch('https://chat-radio.com/songlist')
+    .then(res => {
+      console.log(res)
+      if (!res.ok) {
+        myDB.keys()
+        .then(keys=>{
+          setSongList(keys)
+        })
+        console.log('return?')
+        setSongList('err')
+        throw new Response({ok:false})
+      }
+      console.log('res?')
+      setSongList('done')
+      res.json()
+      .then(json => {
+        console.log('we okay!')
+        let undeletedSongs = []
+        json.forEach(e => {
+          return e.deleted ? null :
+          undeletedSongs.push(e)
+        })
+        console.log(undeletedSongs.length, ' songs fetched')
+        // console.log(undeletedSongs)
+        setSongList(undeletedSongs)
+      })
+    })
+    .catch(err => {
+      setSongList('err')
+      console.error({msg:'offline?', err})
+    })
+}
